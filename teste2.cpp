8f1f04ebadaef4c52010ceba480e55a4b95bfa1a
Ana Clara Fernandes Vieira <ana.vieira.079@ufrn.edu.br>
2025-01-05 23:52:44 -0300

    Update teste2.cpp

--- teste2.cpp
+++ teste2.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+string andAbr = "AN";
+string orAbr = "OR";
+string notAbr = "NT";
+string xorAbr = "XO";
+string nandAbr = "NA";
+string norAbr = "NO";
+string nxorAbr = "NX";
+
+string erro1 = "MX";
+
 // Funcao para imprimir as informacoes sobre um Circuito
 void imprimir(const string& Nome, const Circuito& C)
 {
@@ -28,12 +38,12 @@ Circuito inicializa()
   // Dimensiona o circuito
   prov.resize(2,1,1);
   // Inicializa a porta Id==1 como AND de 2 entradas
-  prov.setPort(1,"AN",2);
+  prov.setPort(1,andAbr,2);
   // As duas entradas da porta Id==1 veem das entradas -1 e -2 do circuito
   prov.setIdInPort(1, 0, -1);
   prov.setIdInPort(1, 1, -2);
   // A saida Id==1 do circuito vem da porta Id==1
-  prov.setIdOutput(1, 1);
+  prov.setIdOutputCirc(1, 1);
 
   return prov;
 }
@@ -44,8 +54,8 @@ int main(void)
   cout << "1)==========\n";
   Circuito C0;
   imprimir("C0",C0);     // Deve imprimir "C0 NIn=0 NOut=0 NPort=0:"
-  if (C0.setIdOutput(1,1)) cerr << "Deveria dar erro\n";
-  if (C0.setPort(1,"AN",2)) cerr << "Deveria dar erro\n";
+  if (C0.setIdOutputCirc(1,1)) cerr << "Deveria dar erro\n";
+  if (C0.setPort(1,andAbr,2)) cerr << "Deveria dar erro\n";
 
   // Testando o resize
   cout << "2)==========\n";
@@ -55,16 +65,16 @@ int main(void)
   // Testando a modificacao de circuito
   cout << "3)==========\n";
   // Inicializa porta inexistente (id invalido)
-  if (C0.setPort(-1,"NO",3)) cerr << "Deveria dar erro 1\n";
-  if (C0.setPort(0,"NO",3)) cerr << "Deveria dar erro 2\n";
-  if (C0.setPort(3,"NO",3)) cerr << "Deveria dar erro 3\n";
+  if (C0.setPort(-1,norAbr,3)) cerr << "Deveria dar erro 1\n";
+  if (C0.setPort(0,norAbr,3)) cerr << "Deveria dar erro 2\n";
+  if (C0.setPort(3,norAbr,3)) cerr << "Deveria dar erro 3\n";
   // Inicializa a porta Id==1 (valores invalidos e validos)
-  if (C0.setPort(1,"MX",3)) cerr << "Deveria dar erro 4\n";
-  if (C0.setPort(1,"NO",1)) cerr << "Deveria dar erro 5\n";
-  if (!C0.setPort(1,"NO",3)) cerr << "Nao deveria dar erro 6\n";
+  if (C0.setPort(1,erro1,3)) cerr << "Deveria dar erro 4\n";
+  if (C0.setPort(1,norAbr,1)) cerr << "Deveria dar erro 5\n";
+  if (!C0.setPort(1,norAbr,3)) cerr << "Nao deveria dar erro 6\n";
   // Inicializa a porta Id==2 (valores invalidos e validos)
-  if (C0.setPort(2,"NT",2)) cerr << "Deveria dar erro 7\n";
-  if (!C0.setPort(2,"NT",1)) cerr << "Nao deveria dar erro 8\n";
+  if (C0.setPort(2,notAbr,2)) cerr << "Deveria dar erro 7\n";
+  if (!C0.setPort(2,notAbr,1)) cerr << "Nao deveria dar erro 8\n";
   // Modifica as origens das entradas das portas
   if (C0.setIdInPort(-1,0,-1)) cerr << "Deveria dar erro 9\n";
   if (C0.setIdInPort(0,0,-1)) cerr << "Deveria dar erro 10\n";
@@ -76,12 +86,12 @@ int main(void)
   if (C0.setIdInPort(2,-1,-2)) cerr << "Deveria dar erro 16\n";
   if (C0.setIdInPort(3,0,-1)) cerr << "Deveria dar erro 17\n";
   // Modifica as origens das saidas
-  if (C0.setIdOutput(-1,1)) cerr << "Deveria dar erro 18\n";
-  if (C0.setIdOutput(0,1)) cerr << "Deveria dar erro 19\n";
-  if (!C0.setIdOutput(1,1)) cerr << "Nao deveria dar erro 20\n";
-  if (C0.setIdOutput(1,0)) cerr << "Deveria dar erro 21\n";
-  if (!C0.setIdOutput(2,-2)) cerr << "Nao deveria dar erro 22\n";
-  if (C0.setIdOutput(3,1)) cerr << "Deveria dar erro 23\n";
+  if (C0.setIdOutputCirc(-1,1)) cerr << "Deveria dar erro 18\n";
+  if (C0.setIdOutputCirc(0,1)) cerr << "Deveria dar erro 19\n";
+  if (!C0.setIdOutputCirc(1,1)) cerr << "Nao deveria dar erro 20\n";
+  if (C0.setIdOutputCirc(1,0)) cerr << "Deveria dar erro 21\n";
+  if (!C0.setIdOutputCirc(2,-2)) cerr << "Nao deveria dar erro 22\n";
+  if (C0.setIdOutputCirc(3,1)) cerr << "Deveria dar erro 23\n";
   imprimir("C0",C0);     // OK - deve imprimir "C0 NIn=2 NOut=2 NPort=2: NO NT"
 
   // Testando o construtor por copia e clear
@@ -101,7 +111,7 @@ int main(void)
   imprimir("C2",C2);     // OK - deve imprimir "C2 NIn=2 NOut=1 NPort=1: AN"
   // Alterando C2
   // Inicializa a porta Id==1 como NXOR de 2 entradas
-  if (!C2.setPort(1,"NX",2)) cerr << "Nao deveria dar erro 25\n";
+  if (!C2.setPort(1,nxorAbr,2)) cerr << "Nao deveria dar erro 25\n";
   // As duas entradas da porta Id==1 veem das entradas -1 e -2 do circuito
   if (!C2.setIdInPort(1, 0, -1)) cerr << "Nao deveria dar erro 26\n";
   if (!C2.setIdInPort(1, 1, -2)) cerr << "Nao deveria dar erro 27\n";

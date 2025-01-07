659cb70024001067e01a43eae3a4cdf0f85d4e94
Ana Clara Fernandes Vieira <ana.vieira.079@ufrn.edu.br>
2025-01-06 21:43:58 -0300

    Update porta.cpp

--- porta.cpp
+++ porta.cpp
@@ -14,16 +14,8 @@
     }
     //Simulador da porta Not
     bool PortaNOT::simular(const std::vector<bool3S>& in_port){
-        if(in_port.size()>0 && in_port.size()==static_cast<size_t>(getNumInputs())){
-            if(in_port[0] == bool3S::TRUE){
-                setOutput(bool3S::FALSE);
-            }
-            else if(in_port[0] == bool3S::FALSE){
-                setOutput(bool3S::TRUE);
-            }
-            else{
-                setOutput(bool3S::UNDEF);
-            }
+        if(in_port.size()==1){
+            setOutput(~in_port[0]);
             return true;
         }
         else{
@@ -44,13 +36,9 @@
     //Simulador da porta And
     bool PortaAND::simular(const std::vector<bool3S>& in_port){
         if(in_port.size()>0 && in_port.size()==static_cast<size_t>(getNumInputs())){
-            int falseCont = 0;
-            for(size_t i=0; i<in_port.size(); ++i){if(in_port[i]==bool3S::FALSE){++falseCont;}};
-            if(falseCont){setOutput(bool3S::FALSE); return true;};
             bool3S result = in_port[0];
-            for(size_t i=1; i<in_port.size(); ++i){
-                if(in_port[i]==bool3S::UNDEF){setOutput(bool3S::UNDEF); return true;}
-                if(in_port[i]==bool3S::FALSE){result=bool3S::FALSE; break;}
+            for (int i = 1; i < getNumInputs(); ++i){
+                result &= in_port[i];
             }
             setOutput(result);
             return true;
@@ -72,17 +60,12 @@
     //Simulador da porta NAnd
     bool PortaNAND::simular(const std::vector<bool3S>& in_port){
         if(in_port.size()>0 && in_port.size()==static_cast<size_t>(getNumInputs())){
-            PortaAND pAnd(in_port.size());
-            if(pAnd.simular(in_port)){
-                bool3S andResult = pAnd.getOutput();
-                if(andResult==bool3S::UNDEF){setOutput(bool3S::UNDEF); return true;}
-                bool3S result = (andResult == bool3S::TRUE)? bool3S::FALSE : bool3S::TRUE;
-                setOutput(result);
-                return true;
-            }else{
-                setOutput(bool3S::UNDEF);
-                return false;
+            bool3S result = in_port[0];
+            for (int i = 1; i < getNumInputs(); ++i){
+                result &= in_port[i];
             }
+            setOutput(~result);
+            return true;
         }
         else{
             setOutput(bool3S::UNDEF);
@@ -102,13 +85,9 @@
     //Simulador da porta Or
     bool PortaOR::simular(const std::vector<bool3S>& in_port){
         if(in_port.size()>0 && in_port.size()==static_cast<size_t>(getNumInputs())){
-            int trueCont = 0;
-            for(size_t i=0; i<in_port.size(); ++i){if(in_port[i]==bool3S::TRUE){++trueCont;}};
-            if(trueCont){setOutput(bool3S::TRUE); return true;};
             bool3S result = in_port[0];
-            for(size_t i=1; i<in_port.size(); ++i){
-                if(in_port[i]==bool3S::UNDEF){setOutput(bool3S::UNDEF); return true;}
-                if(in_port[i]==bool3S::TRUE){result=bool3S::TRUE; break;}
+            for (int i = 1; i < getNumInputs(); ++i){
+                result |= in_port[i];
             }
             setOutput(result);
             return true;
@@ -131,17 +110,12 @@
     //Simulador da porta NOr
     bool PortaNOR::simular(const std::vector<bool3S>& in_port){
         if(in_port.size()>0 && in_port.size()==static_cast<size_t>(getNumInputs())){
-            PortaOR pOr(in_port.size());
-            if(pOr.simular(in_port)){
-                bool3S orResult = pOr.getOutput();
-                if(orResult==bool3S::UNDEF){setOutput(bool3S::UNDEF); return true;}
-                bool3S result = (orResult == bool3S::TRUE)? bool3S::FALSE : bool3S::TRUE;
-                setOutput(result);
-                return true;
-            }else{
-                setOutput(bool3S::UNDEF);
-                return false;
+            bool3S result = in_port[0];
+            for (int i = 1; i < getNumInputs(); ++i){
+                result |= in_port[i];
             }
+            setOutput(~result);
+            return true;
         }
         else{
             setOutput(bool3S::UNDEF);
@@ -160,14 +134,10 @@
     //Simulador da porta Xor
     bool PortaXOR::simular(const std::vector<bool3S>& in_port){
         if(in_port.size()>0 && in_port.size()==static_cast<size_t>(getNumInputs())){
-            if(in_port[0] == bool3S::UNDEF){setOutput(bool3S::UNDEF); return true;}
             bool3S result = in_port[0];
-            int TCounter = (result==bool3S::TRUE)? 1:0;
-            for(size_t i=1; i<in_port.size(); ++i){
-                if(in_port[i]==bool3S::UNDEF){setOutput(bool3S::UNDEF);return false;}
-                if(in_port[i]==bool3S::TRUE){TCounter++;}
+            for (int i = 1; i < getNumInputs(); ++i){
+                result ^= in_port[i];
             }
-            result = (TCounter%2==1)? bool3S::TRUE : bool3S::FALSE;
             setOutput(result);
             return true;
         }
@@ -189,17 +159,12 @@
     //Simulador da porta NXor
     bool PortaNXOR::simular(const std::vector<bool3S>& in_port){
         if(in_port.size()>0 && in_port.size()==static_cast<size_t>(getNumInputs())){
-            PortaXOR pXor(in_port.size());
-            if(pXor.simular(in_port)){
-                bool3S xorResult = pXor.getOutput();
-                if(xorResult==bool3S::UNDEF){setOutput(bool3S::UNDEF); return true;}
-                bool3S result = (xorResult == bool3S::TRUE)? bool3S::FALSE : bool3S::TRUE;
-                setOutput(result);
-                return true;
-            }else{
-                setOutput(bool3S::UNDEF);
-                return false;
+            bool3S result = in_port[0];
+            for (int i = 1; i < getNumInputs(); ++i){
+                result ^= in_port[i];
             }
+            setOutput(~result);
+            return true;
         }
         else{
             setOutput(bool3S::UNDEF);
